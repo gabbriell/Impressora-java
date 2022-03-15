a310dc03c50211b2ea1cd7242d399c928b5d2f89
gabriel.javax <gabriel.javax@gmail.com>
2022-03-15 00:44:14 -0300

    src

---
+++ src/controller/ImpressoraController.java
@@ -0,0 +1,82 @@
+package controller;
+
+import helper.ImpressoraHelper;
+import impressora.Impressao;
+import java.awt.Desktop;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.print.Doc;
+import javax.print.DocFlavor;
+import javax.print.DocPrintJob;
+import javax.print.PrintException;
+import javax.print.PrintService;
+import javax.print.PrintServiceLookup;
+import javax.print.ServiceUI;
+import javax.print.SimpleDoc;
+import javax.print.attribute.DocAttributeSet;
+import javax.print.attribute.HashDocAttributeSet;
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
+import javax.print.attribute.standard.Copies;
+import javax.print.attribute.standard.MediaSize;
+import javax.swing.JOptionPane;
+import view.ViewCapaProcesso;
+
+
+
+/**
+ *
+ * @author gabrielm
+ */
+public class ImpressoraController {
+    
+    private final ViewCapaProcesso view;
+    private ImpressoraHelper helper;
+    private Impressao impressora;
+    private DocAttributeSet hasDoc;
+
+    public ImpressoraController(ViewCapaProcesso view) {
+        this.view = view;
+        this.helper = new ImpressoraHelper(view);
+        
+    }
+    
+    public void imprimirCapa() throws FileNotFoundException{
+        
+        impressora = helper.obterModelo();
+        JOptionPane.showMessageDialog(view,"Imprimindo: " + impressora.getConteudo1());
+             
+       
+        PrintService[] printService = PrintServiceLookup.lookupPrintServices(DocFlavor.INPUT_STREAM.AUTOSENSE,null);
+        PrintService impressoraPadrao = PrintServiceLookup.lookupDefaultPrintService();
+        DocFlavor docFlavor = DocFlavor.INPUT_STREAM.AUTOSENSE;
+        HashDocAttributeSet hashDocAttributeSet = new HashDocAttributeSet();
+        
+        try{
+            InputStream InputStream = new ByteArrayInputStream(impressora.getConteudo1().getBytes());
+            Doc doc = new SimpleDoc(InputStream, docFlavor,hashDocAttributeSet);
+            PrintRequestAttributeSet printRequestAttributeSet = new HashPrintRequestAttributeSet();
+            PrintService printServico = ServiceUI.printDialog(null, 300, 200, printService, impressoraPadrao, docFlavor, printRequestAttributeSet);
+            if(printServico != null){
+                DocPrintJob docPrintJob = printServico.createPrintJob();
+                
+                //mandar imprimir documento
+                docPrintJob.print(doc, printRequestAttributeSet);
+            }
+        } catch (PrintException ex) {
+            JOptionPane.showMessageDialog(view, ex);
+        }
+        
+        //helper.limparTela();
+        
+      
+    
+    }
+
+  
+    
+}
